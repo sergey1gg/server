b696158b2e083d3c3d89e0bc77dc7ba809410102
unknown <sergeyleb66@mail.ru>
2023-05-13 14:21:40 +0300

    n commit

--- models/Directory.js
+++ models/Directory.js
@@ -2,29 +2,40 @@ const {Schema,model}=require('mongoose')
 
 const Directory = new Schema({
    rooms: {
-    type: [String],
-    default: ["спальня","кухня","туалет","ванная","гардероб"]
+     type: [{
+       name: String,
+       options: [String]
+     }],
+
    },
    elements: {
-    type: [String],
-    default: ["потолок","входная дверь","пол"]
+     type: [{
+       name: String,
+       options: [String]
+     }],
+    
    },
    otdelka: {
-    type: [String],
-    default: ["паркет","ламинат","обои"]
+     type: [{
+       name: String,
+       options: [String]
+     }],
+     
    },
    defects: {
-    type: [String],
-    default: ["повреждение профиля","повреждение рамы"]
+     type: [{
+       name: String,
+       options: [String]
+     }],
+    
    },
    subdefects: {
-    type: [String],
-    default: ["скол","пробит","прожжен"]
+     type: [{
+       name: String,
+       options: [String]
+     }],
+     
    },
-   comments: {
-      type: [String],
-      default: ["Внутренний откос имеет механические дефекты (СП 71.13330.2017 п. 7.2.13)","Визуальный осмотр всех оконных ручек. Повреждения любого характера не допускаются"]
-     },
 })
 
 module.exports =model('Directory', Directory)
--- routes/directoryRouter.js
+++ routes/directoryRouter.js
@@ -16,5 +16,38 @@ router.get('/get', async (req, res) => {
     }
   });
 
+
+  router.post('/set', async (req, res) => {
+    try {
+      const { data } = req.body;
+      console.log(data)
+      // Проверка наличия обязательного поля data
+      if (!data) {
+        return res.status(400).json({ error: 'Missing data field' });
+      }
+  
+      // Удаление существующей директории
+      await Directory.deleteOne({});
+
+    // Создание и сохранение новой директории
+    const newDirectory = new Directory();
+
+    // Сохранение значений из req.body в схему
+    newDirectory.rooms = data.rooms.default;
+    newDirectory.elements = data.elements.default;
+    newDirectory.otdelka = data.otdelka.default;
+    newDirectory.defects = data.defects.default;
+    newDirectory.subdefects = data.subdefects.default;
+
+    // Сохранение новой директории
+    await newDirectory.save();
+
+
+    return res.json(newDirectory);
+    } catch (error) {
+      console.error('Error updating directory:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 module.exports=router
   
